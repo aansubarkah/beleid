5d14a45ee983b0a1195f37b50c077fdfaa7b8c41
aansubarkah <aansubarkah@gmail.com>
2014-09-15 00:56:58 +0700

    initial commit

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '41ngDYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9miP154n');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '1276859309657453542496749683645022012');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
